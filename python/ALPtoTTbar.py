600ffc76d630b69283e415576d42464ece24dfdc
Henrik Jabusch <henrik.jabusch@uni-hamburg.de>
2022-11-25 16:49:07 +0100

    move minus sign to physics model

--- python/ALPtoTTbar.py
+++ python/ALPtoTTbar.py
@@ -10,29 +10,10 @@ class ALPtoTTbar(PhysicsModel):
 
     def doParametersOfInterest(self):
         """Create POI and other parameters, and define the POI set."""
-        self.modelBuilder.doVar("r[1,0,20]");
+        self.modelBuilder.doVar("r[1,0,20]")
         self.modelBuilder.doSet("POI", "r")
 
         self.modelBuilder.factory_("expr::signal_func(\"@0\",r)")
-        self.modelBuilder.factory_("expr::interference_func(\"sqrt(@0)\",r)")
+        self.modelBuilder.factory_("expr::interference_func(\"-sqrt(@0)\",r)")
 
 alpttottbar = ALPtoTTbar()
-
-
-
-
-
-# def __init__(self):
-#     self.c_G   = 1.0 # gg -> a coupling (default = 1)
-#     self.c_Phi = 1.0 # a -> ttbar coupling (default = 1)
-
-# def setPhysicsOptions(self,physOptions):
-#     for po in physOptions:
-#         if po.startswith("c_G="):
-#             self.c_G = float(po.replace("c_G=", ""))
-#         if po.startswith("c_Phi="):
-#             self.c_Phi = float(po.replace("c_Phi=", ""))
-
-# self.modelBuilder.factory_("expr::C(\"abs(1*1)\")")
-# self.modelBuilder.factory_("expr::C(\"abs(@0*@1)\"), c_G, c_Phi")
-# self.modelBuilder.factory_("expr::r(\"pow(1.0/pow(@1,2),2)\"), f_a")
