b0c4ce65650cfc553690d85760cecf80ada00255
jabuschh <henrik.jabusch@uni-hamburg.de>
2023-10-10 15:13:43 +0200

    add pulls without statistical uncertainties

--- B2G-22-006/05b_np_pulls_noStats.py
+++ B2G-22-006/05b_np_pulls_noStats.py
@@ -14,11 +14,11 @@ years = [
 ]
 
 scenarios = [
-    'negint',
+    # 'negint',
     'posint'
 ]
 
-pulls_dir_name = 'pulls/'
+pulls_dir_name = 'pulls_noStats/'
 if not os.path.exists(pulls_dir_name):
     os.mkdir(pulls_dir_name)
 
@@ -37,30 +37,29 @@ for scenario in scenarios:
 
     print('running fit...')
     command_step1 = 'combineTool.py -M FitDiagnostics' # method
-    command_step1 += ' -d ../../asymptoticlimits/workspace_' + year_to_run + '_fa' + fa + '_' + scenario + '.root' # workspace
+    command_step1 += ' -d ../../asymptoticlimits/workspace_' + year_to_run + '_fa' + fa + '_noStats_' + scenario + '.root' # workspace
     command_step1 += ' -m 125' # higgs mass
     command_step1 += ' -t -1' # Asimov data
     # command_step1 += ' --expectSignal 0' # --rMin -5 --rMax 5
     command_step1 += ' --setParameterRanges ttag_corr=-5,5' + ''.join(':ttag_uncorr_' + year + '=-5,5' for year in years)
-    command_step1 += ' -n _' + scenario
+    command_step1 += ' -n _noStats_' + scenario
     print(command_step1)
-    log_step1 = subprocess.run(['bash', '-c', command_step1], capture_output=True)
-    log_file1 = pulls_log_dir_name + 'fit_' + year_to_run + '_fa' + fa + '_' + scenario + '.log'
-    with open(log_file1, 'w') as file:
-        file.write(log_step1.stdout.decode())
+    # log_step1 = subprocess.run(['bash', '-c', command_step1], capture_output=True)
+    # log_file1 = pulls_log_dir_name + 'fit_' + year_to_run + '_fa' + fa + '_noStats_' + scenario + '.log'
+    # with open(log_file1, 'w') as file:
+    #     file.write(log_step1.stdout.decode())
 
 
     print('plotting...')
     command_step2 = 'python3 ../../../test/diffNuisances.py'
-    command_step2 += ' fitDiagnostics_' + scenario + '.root'
+    command_step2 += ' fitDiagnostics_noStats_' + scenario + '.root'
     # command_step2 += ' --abs --all'
-    command_step2 += ' -g pulls_' + scenario + '.root'
+    command_step2 += ' -g pulls_noStats_' + scenario + '.root'
     print(command_step2)
-    log_step2 = subprocess.run(['bash', '-c', command_step2], capture_output=True)
-    log_file2 = pulls_log_dir_name + 'plotting_' + year_to_run + '_fa' + fa + '_' + scenario + '.log'
-    with open(log_file2, 'w') as file:
-        file.write(log_step2.stdout.decode())
-
+    # log_step2 = subprocess.run(['bash', '-c', command_step2], capture_output=True)
+    # log_file2 = pulls_log_dir_name + 'plotting_' + year_to_run + '_fa' + fa + '_noStats_' + scenario + '.log'
+    # with open(log_file2, 'w') as file:
+    #     file.write(log_step2.stdout.decode())
 
     os.chdir('..')
 
